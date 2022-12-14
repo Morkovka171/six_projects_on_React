9f4a272391a6587dd9c81d83c48354ee032049e0
Morkovka171 <guganowa@yandex.ru>
2022-12-14 19:53:21 +0300

    Сделано модальное окно

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import Counter from './projects/Counter/index'
+import React from 'react';
+// import Counter from './projects/Counter/index';
+import Modal from './projects/Modal/index';
 
 const App = () => {
   return (
-    <Counter/>
+    <>
+      {/* <Counter /> */}
+      <Modal />
+    </>
   )
 }
 
-export default App
+export default App;
--- src/projects/Counter/index.js
+++ src/projects/Counter/index.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import './style.scss';
+import React, { useState } from 'react';
+import './index.scss';
 
 const Counter = () => {
-    const [count, setCount] = React.useState(0);
 
-    const onClickPlus = () => {
-        setCount(count + 1);
+    const [count, setCount] = useState(0);
+    const [counterColor, setCounterColor] = useState('#000');
+
+    const changeCounter = (number) => {
+        setCount(count + number);
+        setCounterColor(number < 0 ? '#e63946' : '#2a9d8f');
     }
 
-    const onClickMinus = () => {
-        setCount(count - 1);
+    const resetCounter = () => {
+        setCount(0); 
+        setCounterColor('#000');
     }
 
     return (
         <div className="App">
             <div>
                 <h2>Счетчик:</h2>
-                <h1>{count}</h1>
-                <button onClick={onClickMinus} className="minus">- Минус</button>
-                <button onClick={onClickPlus} className="plus">Плюс +</button>
+                <h1 style={{color: counterColor}}>{count}</h1>
+                <div>
+                    <button onClick={() => changeCounter(-1)} className="minus">- 1</button>
+                    <button onClick={() => changeCounter(1)} className="plus">+ 1</button>
+                </div>
+                <div>
+                    <button onClick={() => changeCounter(-10)} className="minus">- 10</button>
+                    <button onClick={() => changeCounter(10)} className="plus">+ 10</button>
+                </div>
+                <div>
+                    <button onClick={() => changeCounter(-100)} className="minus">- 100</button>
+                    <button onClick={() => changeCounter(100)} className="plus">+ 100</button>
+                </div>
+                <div>
+                    <button onClick={() => changeCounter(-1000)} className="minus">- 1000</button>
+                    <button onClick={() => changeCounter(1000)} className="plus">+ 1000</button>
+                </div>
+                <div>
+                    <button onClick={resetCounter} className="reset">0</button>
+                </div>
             </div>
         </div>
     );
