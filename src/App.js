17f1db5aeb96ac0e545f9adf5a22e5e4be1cbe0d
Morkovka171 <guganowa@yandex.ru>
2022-12-15 11:42:07 +0300

    Сделала Quiz (опросник)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 // import Counter from './projects/Counter/index';
-import Modal from './projects/Modal/index';
+// import Modal from './projects/Modal/index';
+import Quiz from './projects/Quiz/index';
+
 
 const App = () => {
   return (
     <>
       {/* <Counter /> */}
-      <Modal />
+      {/* <Modal /> */}
+      <Quiz />
     </>
   )
 }
