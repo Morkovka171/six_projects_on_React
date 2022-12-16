a13e8077ee520114ac8020ab6ab3bb457346ccde
Morkovka171 <guganowa@yandex.ru>
2022-12-16 17:24:48 +0300

    Список пользователей

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 // import Counter from './projects/Counter/index';
 // import Modal from './projects/Modal/index';
-import Quiz from './projects/Quiz/index';
-
+// import Quiz from './projects/Quiz/index';
+import Userss from './projects/Users/index';
 
 const App = () => {
   return (
     <>
       {/* <Counter /> */}
       {/* <Modal /> */}
-      <Quiz />
+      {/* <Quiz /> */}
+      <Userss />
     </>
   )
 }
--- src/projects/Quiz/index.js
+++ src/projects/Quiz/index.js
@@ -23,56 +23,57 @@ const questions = [
   },
 ];
 
-function Result({correct}) {
+function Result({ correct }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
       <a href='/'>
-      <button>Попробовать снова</button>
+        <button>Попробовать снова</button>
       </a>
     </div>
   );
 }
 
-function Game({step, question, onClickVariant}) {
-    const persentage = Math.round((step / questions.length) * 100);
+function Game({ step, question, onClickVariant }) {
+  const persentage = Math.round((step / questions.length) * 100);
 
-    return(
-        <>
-        <div className="progress">
-            <div style={{ width: `${persentage}%` }} className="progress__inner"></div>
-        </div>
-        <h1>{question.title}</h1>
-        <ul>
-            {
-                question.variants.map((text, index) => 
-                <li onClick={() => onClickVariant(index)} key={text}> {text}</li>)
-            }
-        </ul>
-        </>
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${persentage}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {
+          question.variants.map((text, index) =>
+            <li onClick={() => onClickVariant(index)} key={text}>
+              {text}
+            </li>)
+        }
+      </ul>
+    </>
   );
 }
 
 function Quiz() {
-    const [step, setStep] = React.useState(0);
-    const [correct, setCorrect] = React.useState(0);
-    const question = questions[step];
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = questions[step];
 
-    const onClickVariant = (index) => { 
-      console.log(step, index);
-      setStep(step+1);
-      if(index === question.correct) {
-        setCorrect(correct + 1);
-      }
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+    if (index === question.correct) {
+      setCorrect(correct + 1);
     }
+  }
   return (
     <div className="App">
-    {step !== questions.length ? ( 
-        <Game step={step} question={question} onClickVariant={onClickVariant} /> 
-    ) : (
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
         <Result correct={correct} />
-    )}
+      )}
     </div>
   );
 }
